f0bb0a2225a6097d2e629ee020ee26ac4faf6b0b
Patrick Weaver <pjpwea@gmail.com>
2017-11-13 15:35:47 -0500

    update 3rd round

--- script.js
+++ script.js
@@ -28,15 +28,15 @@ $(document).ready(function(){
     ],
     [
       [
-        "Hi all! We're excited to invite interested folks from the Hive NYC community to the next CS-Paths Working Group session. This month, we’ll be hearing from the CS-Path project’s three design partners about the pathway building and brokering tools they’re developing, how iterations of their designs have unfolded, and early data and emergent lessons from design research cycles.", "round3option0.png", "real"
+        "", "instagram1.png", "scam"
 
       ],
       [
-        "Hi Patrick, In 2 weeks from now we'll be joined by industry thought leaders from Centene, Amway and HM Health Solutions for an insightful webinar discussion on how to leverage L&D along with digital solutions to overcome the challenges imposed by the ongoing changes in the marketplace. Save your complimentary spot and join us on Thursday, April 6th at 2pm EDT as our panelists explore: industry trends in a rapidly changing worlds, ultural and strategic alignment to achieve organizational objective, technology to simplify learning, continuous performance improvements . We know you have a busy schedule. Register even if you can't attend and I'll make sure you receive a copy of the recording post-webinar.", "round3option1.png", "scam"
+        "", "instagram2.png", "real"
 
       ],
       [
-        "Welcome to Linode! Please confirm your email address. You're almost there! Please click the link below to create your Linode Manager account. Confirm my email and create my account! » Here are a few things to help you get started: Linode Docs - Getting Started Linode Docs - Beginner's Guide Join us via webchat or via #linode on irc.oftc.net For site migrations, server installs, one-off sysadmin tasks and nearly anything else you need, check out our Professional Services team.", "round3option2.png", "real"
+        "", "instagram3.png", "real"
       ]
     ]
   ]
