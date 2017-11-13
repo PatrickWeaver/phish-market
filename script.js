581ff3e1bedf244df72b05d810f637dc961b6b3c
Patrick Weaver <pjpwea@gmail.com>
2017-11-13 17:59:02 -0500

    layout changes

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ $(document).ready(function(){
   var options;
   var realChosen = 0;
   var scamsChosen = 0;
+  var position = 0;
 
   function nextOptions() {
     options = $( ".option-box" );
@@ -69,6 +70,49 @@ $(document).ready(function(){
     $( "#game" ).show();
     nextOptions();
   });
+
+  //var optionsWidth = $( "#option-boxes-container" ).outerWidth();
+
+
+
+  $( "#right-button" ).click(function() {
+    var optionsWidth = $(window).width()
+    if (position > -2){
+      var cardWidth = optionsWidth + 4;
+      var currentLeftVal = $( "#option-boxes-container" ).css("left");
+      var currentLeft = parseInt(currentLeftVal.substring(0, currentLeftVal.length - 2));
+      $( "#option-boxes-container" ).animate({left: (currentLeft - cardWidth).toString() + "px" }, 1000, function() {
+        position += 1;
+        showCorrectButtons();
+      });
+    }
+  });
+
+  $( "#left-button" ).click(function() {
+    var optionsWidth = $(window).width()
+    if (position > 0){
+      var cardWidth = optionsWidth + 4;
+      var currentLeftVal = $( "#option-boxes-container" ).css("left");
+      var currentLeft = parseInt(currentLeftVal.substring(0, currentLeftVal.length - 2));
+      $( "#option-boxes-container" ).animate({left: (currentLeft + cardWidth).toString() + "px" }, 1000, function() {
+        position -= 1;
+        showCorrectButtons();
+      });
+    }
+  });
+
+  function showCorrectButtons() {
+    if (position === 1) {
+      $( "#right-button").show();
+      $( "#left-button" ).show();
+    }
+    if (position === 0) {
+      $( "#left-button" ).hide();
+    }
+    if (position === 2) {
+      $( "#right-button" ).hide();
+    }
+  }
 
 
   $( ".option-box").click(function() {
